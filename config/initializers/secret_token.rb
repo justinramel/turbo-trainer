e57af7dd3f98a9a9d97922a97309a724938a2cf7
Justin Ramel <justin.ramel@gmail.com>
2012-03-15 23:35:21 +0000

    generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurboTrainer::Application.config.secret_token = '24923c6bb359e096ef6083e48f879805bdac6a3caa8d9a71b641291230d5f991443ee12a6be5142230828f07883673d0d097b152310d730abe93d6024155e608'
